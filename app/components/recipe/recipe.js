75e07f201598ee9fcd987ce1c88e60f75b7c97f6
Iarl <voznyyy@outlook.com>
2016-09-19 16:00:47 +0000

    small fixes

--- app/components/recipe/recipe.js
+++ app/components/recipe/recipe.js
@@ -3,7 +3,7 @@ angular.module('myApp')
         template: `<div><h1>{{$ctrl.recipe.title}}</h1>
                     <p>{{$ctrl.recipe.text}}</p>
                     </div>
-                    <div><button ng-click="$ctrl.editRecipe($ctrl.recipe.id)"></button></div>`,
+                    <div><button ng-click="$ctrl.editRecipe($ctrl.recipe.id)">Save</button></div>`,
         comtroller: function(){
             this.editRecipe = function(id){
                 
